087aac654395def575d6664101b21e672d42a657
shogotsuneto <sh.tsne@gmail.com>
2019-08-17 01:50:55 +0900

    Add md fetching

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <Container />
+      <Container owner='shogotsuneto' repo="md-contents" />
     </div>
   );
 }
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from '../styles/Shell.module.css'
 
-const Container = () => (
-  <div className={styles.container}>あああ</div>
-)
+const Container = ({owner, repo}) => {
+  const [text, setText] = useState('')
+  useEffect(() => {
+    fetch(`https://raw.githubusercontent.com/${owner}/${repo}/master/README.md`)
+      .then(response => response.text())
+      .then(data => setText(data))
+  }, [owner, repo])
+  return <div className={styles.container}>{text}</div>
+}
 
 export default Container
