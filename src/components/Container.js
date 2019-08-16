078bd0cab539b8f1352d148fe96d32ab2fef2199
shogotsuneto <sh.tsne@gmail.com>
2019-08-17 01:13:05 +0900

    Initialize app

---
+++ src/components/Container.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../styles/Shell.module.css'
+
+console.log(styles)
+
+const Container = () => (
+  <div className={styles.container}>あああ</div>
+)
+
+export default Container
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../styles/Shell.module.css'
+
+const Header = () => (
+  <header className={styles.header}>アプリだよ</header>
+)
+
+export default Header
