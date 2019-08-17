8322099df4b2ed3e934fa60c0cfe4d3b2bda1d33
shogotsuneto <sh.tsne@gmail.com>
2019-08-17 18:00:37 +0900

    Change sample md

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import unified from 'unified'
 import parse from 'remark-parse'
+import remarkBreaks from 'remark-breaks'
 import remarkAozoraRuby from 'remark-aozora-ruby'
 import remark2rehype from 'remark-rehype'
+import sanitize from 'rehype-sanitize'
 import rehype2react from 'rehype-react'
 import styles from '../styles/Shell.module.css'
 
@@ -10,7 +12,9 @@ const createMarkDownElements = text => (
   unified()
     .use(parse)
     .use(remarkAozoraRuby)
+    .use(remarkBreaks)
     .use(remark2rehype)
+    .use(sanitize)
     .use(rehype2react, { createElement: React.createElement})
     .processSync(text).contents
 )
@@ -18,7 +22,7 @@ const createMarkDownElements = text => (
 const Container = ({owner, repo}) => {
   const [text, setText] = useState('')
   useEffect(() => {
-    fetch(`https://raw.githubusercontent.com/${owner}/${repo}/master/README.md`)
+    fetch(`https://raw.githubusercontent.com/${owner}/${repo}/master/kokoro.md`)
       .then(response => response.text())
       .then(data => setText(data))
   }, [owner, repo])
