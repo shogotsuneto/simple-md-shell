0b7aefd4e57a43a8dfaa6983c7731629bdde1bd3
shogotsuneto <sh.tsne@gmail.com>
2019-08-19 17:40:27 +0900

    Add sectionize

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,32 +1,41 @@
 import React, { useState, useEffect } from 'react'
 import unified from 'unified'
 import parse from 'remark-parse'
+import sectionize from 'remark-sectionize'
 import remarkBreaks from 'remark-breaks'
 import remarkAozoraRuby from 'remark-aozora-ruby'
 import remark2rehype from 'remark-rehype'
 import sanitize from 'rehype-sanitize'
+import gh from 'hast-util-sanitize/lib/github'
 import rehype2react from 'rehype-react'
 import styles from '../styles/Shell.module.css'
+
+const schema = { ...gh, tagNames: [...gh.tagNames, 'section']}
 
 const createMarkDownElements = text => (
   unified()
     .use(parse)
+    .use(sectionize)
     .use(remarkAozoraRuby)
     .use(remarkBreaks)
     .use(remark2rehype)
-    .use(sanitize)
+    .use(sanitize, schema)
     .use(rehype2react, { createElement: React.createElement})
-    .processSync(text).contents
+    .process(text)
 )
 
 const Container = ({owner, repo}) => {
   const [text, setText] = useState('')
+  const [contents, setContent] = useState('')
   useEffect(() => {
     fetch(`https://raw.githubusercontent.com/${owner}/${repo}/master/kokoro.md`)
       .then(response => response.text())
       .then(data => setText(data))
   }, [owner, repo])
-  return <div className={styles.container}>{createMarkDownElements(text)}</div>
+  useEffect(() => {
+    createMarkDownElements(text).then(({contents}) => setContent(contents))
+  }, [text])
+  return <div className={styles.container}>{contents}</div>
 }
 
 export default Container
