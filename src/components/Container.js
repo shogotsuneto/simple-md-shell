bfacf24765cd0bf4738591c1f2dd906bb4af1c96
shogotsuneto <sh.tsne@gmail.com>
2019-08-21 00:15:20 +0900

    Inplement node tree to react component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from 'react'
+import Tree from './contents/Tree'
 import parseMarkdown from '../processor/parseMarkdown'
 import styles from '../styles/Shell.module.css'
 
 const Container = ({owner, repo}) => {
   const [raw, setRaw] = useState('')
+  const [node, setNode] = useState({})
   useEffect(() => {
     fetch(`https://raw.githubusercontent.com/${owner}/${repo}/master/kokoro.md`)
       .then(response => response.text())
       .then(data => setRaw(data))
   }, [owner, repo])
   useEffect(() => {
-    parseMarkdown.run(parseMarkdown.parse(raw)).then(node => console.log(node))
+    parseMarkdown.run(parseMarkdown.parse(raw)).then(node => {
+      console.log(node)
+      setNode(node)
+    })
   }, [raw])
-  return <div className={styles.container}>{raw}</div>
+  return <div className={styles.container}><Tree node={node} /></div>
 }
 
 export default Container
---
+++ src/components/contents/Tree.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Section from './Section'
+import Heading from './Heading'
+
+const components = {
+  root: ({children}) => <React.Fragment>{renderChildren(children)}</React.Fragment>,
+  section: ({children}) => <Section>{renderChildren(children)}</Section>,
+  heading: ({depth, children}) => <Heading depth={depth}>{renderChildren(children)}</Heading>,
+  text: ({value}) => `${value}`,
+  paragraph: ({children}) => <p>{renderChildren(children)}</p>,
+  ruby: ({base, text}) => <ruby>{base}<rt>{text}</rt></ruby>,
+  break: () => <br />
+}
+
+const renderChildren = children => children.map((node, index) => <Tree key={`${index}`} node={node} />)
+
+const Tree = ({node}) => {
+  return components[node.type] ? React.createElement(components[node.type], {...node}) : null
+}
+
+export default Tree
