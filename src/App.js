4cb3d4416db03cde08510b70cc5ddd88eb350e9f
shogotsuneto <sh.tsne@gmail.com>
2019-08-31 13:56:48 +0900

    Add react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import React, { useState } from 'react';
+import React from 'react'
+import { HashRouter, Route } from 'react-router-dom'
 
 import Header from './components/Header'
-import TableOfContents from './components/TableOfContents';
+import TableOfContents from './components/TableOfContents'
 import Container from './components/Container'
 
-const owner = "shogotsuneto"
-const repo = "md-contents"
-const branch = "master"
-const initialPath = "README.md"
+const owner = 'shogotsuneto'
+const repo = 'md-contents'
+const branch = 'master'
+const initialPath = 'README.md'
 
 function App() {
-  const [current, setCurrent] = useState(initialPath)
   return (
-    <div className="App">
+    <HashRouter>
       <Header />
-      <TableOfContents owner={owner} repo={repo} branch={branch} setCurrent={path => setCurrent(path)} />
-      <Container owner={owner} repo={repo} branch={branch} filepath={current} />
-    </div>
-  );
+      <Route
+        path='/'
+        render={({ history }) => (
+          <TableOfContents
+            owner={owner}
+            repo={repo}
+            branch={branch}
+            setCurrent={path => history.push(path)}
+          />
+        )}
+      />
+      <Route
+        exact
+        path='/'
+        render={() => (
+          <Container
+            owner={owner}
+            repo={repo}
+            branch={branch}
+            filepath={initialPath}
+          />
+        )}
+      />
+      <Route
+        path='/:filepath'
+        render={({ match }) => (
+          <Container
+            owner={owner}
+            repo={repo}
+            branch={branch}
+            filepath={match.params.filepath}
+          />
+        )}
+      />
+    </HashRouter>
+  )
 }
 
-export default App;
+export default App
