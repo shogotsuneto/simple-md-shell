447cb5d7b6abc9bec2d969befa5d76e7f085149d
shogotsuneto <sh.tsne@gmail.com>
2019-08-31 16:26:28 +0900

    Add loader overlay

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,21 +1,42 @@
 import React, { useState, useEffect } from 'react'
-import { fetchRawContent} from '../api'
+import LoadingOverlay from 'react-loading-overlay'
+import { fetchRawContent } from '../api'
 import Tree from './contents/Tree'
 import processor from '../processor/markdownProcessor'
 import styles from '../styles/Shell.module.css'
 
-const Container = ({filepath}) => {
+const Container = ({ filepath }) => {
   const [raw, setRaw] = useState('')
   const [node, setNode] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+
   useEffect(() => {
+    setIsLoading(true)
     fetchRawContent(filepath).then(data => setRaw(data))
   }, [filepath])
   useEffect(() => {
     processor.run(processor.parse(raw)).then(node => {
       setNode(node)
+      setIsLoading(false)
     })
   }, [raw])
-  return <div className={styles.container}><Tree node={node} /></div>
+
+  return (
+    <LoadingOverlay
+      className={styles.container}
+      active={isLoading}
+      fadeSpeed={300}
+      spinner
+      text='Loading your content...'
+      styles={{
+        content: {
+          margin: '200pt auto'
+        }
+      }}
+    >
+      <Tree node={node} />
+    </LoadingOverlay>
+  )
 }
 
 export default Container
